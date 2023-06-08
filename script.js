3baac692a9615e3251acc3b3707f0bea51fcf576
khushal794 <khushals794@gmail.com>
2023-06-08 13:24:08 +0530

    Khushal case study 1

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+  function myFunction() {
+    console.log("Khushal Sharma")
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      let dropdowns = document.getElementsByClassName("dropdown-content");
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+  function saveInput() {
+    let mobileNumber = document.getElementById("mobile-number").value;
+    let emailID = document.getElementById("email-id").value;
+    let panNumber = document.getElementById("pan-number").value;
+    let aadharNumber = document.getElementById("aadhar-number").value;
+    let name = document.getElementById("name").value;
+}
+
+
+
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
